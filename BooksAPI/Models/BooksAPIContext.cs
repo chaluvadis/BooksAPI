0602c48d5098ca3e6c0227295f218dec2fac7c93
chaluvadis <surendra.chaluvadi@gmail.com>
2017-04-30 11:40:20 +0400

    initial code commit

---
+++ BooksAPI/Models/BooksAPIContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAPI.Models
+{
+    public class BooksAPIContext : DbContext
+    {
+        public BooksAPIContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
